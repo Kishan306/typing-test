a8b5ef441e5bedbfa0a201b02b3277a27b7caf0e
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2025-04-02 13:26:42 +0530

    remove outline from focused input field

--- typing-client/src/SocketDisplay.jsx
+++ typing-client/src/SocketDisplay.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:5000");
 
 export default function SocketDisplay() {
-  const [string, setString] = useState("wait for the string");
+  const [string, setString] = useState(null);
   const [value, setValue] = useState('')
   const [score, setScore] = useState(0)
   const [isMatched, setIsMatched] = useState(false)
@@ -37,17 +37,19 @@ export default function SocketDisplay() {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <h1 className="mt-4 text-5xl font-semibold text-balance sm:text-7xl tracking-wider">
+        { string ? (
+          <h1 className="mt-4 text-5xl font-semibold text-balance sm:text-7xl tracking-wider">
           {string.split('').map((char, index)=> (
             <span key={index} className={`${getCharacterStyle(char, index)}`}>
               {char}
             </span>
           ))}
         </h1>
+        ) : <h1 className="mt-4 text-5xl font-semibold text-balance sm:text-7xl">Wait for the string</h1>}
 
         {isMatched ? <p className="text-green-500 italic mt-8 text-lg">Matched!!!</p> : <p className="text-amber-500 italic mt-8 text-lg">Matching...</p>}
 
-        <input value={value} onChange={(e)=> setValue(e.target.value)} className="border-gray-200 bg-gray-200 rounded-full px-4 py-2 mt-6"/>
+        <input value={value} onChange={(e)=> setValue(e.target.value)} className=" bg-gray-200 rounded-full px-4 py-2 mt-6 border-none outline-none"/>
 
         <p className="mt-8">your score: <span className="text-2xl font-semibold">{score}</span></p>
       </div>
